b3b54b7b3e236f681efe2c1c1ee1b9a7ec712551
Ryan <ryandefost@hotmail.com>
2021-12-09 12:28:59 +0100

    Player gets pushed down with Numpad7 and fully works

--- Assets/scripts/movement2.cs
+++ Assets/scripts/movement2.cs
@@ -2,94 +2,95 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace movement2
+public class movement2 : MonoBehaviour
 {
+    private PushDownAttack _pushDownAttack;
+
+    private SpriteRenderer spriteRenderer;
+    public Sprite sprite1;
+    public Sprite sprite2;
+    public float jumpforce;
+    [SerializeField] Rigidbody2D rb;
+    public bool isGrounded { get; private set; }
+    [SerializeField] Rigidbody2D gravity;
+    public bool redmode { get; set; }
+    [SerializeField] public float speed;
 
 
-
-     
-    public class movement2 : MonoBehaviour
+    // Start is called before the first frame update
+    void Start()
     {
-        private SpriteRenderer spriteRenderer;
-        public Sprite sprite1;
-        public Sprite sprite2; 
-        public float jumpforce;
-        [SerializeField] Rigidbody2D rb;
-        private bool isGrounded;
-        [SerializeField] Rigidbody2D gravity;
-        bool redmode = true;
-        [SerializeField] public float speed;
+        redmode = true;
 
+        _pushDownAttack = FindObjectOfType<PushDownAttack>();
 
-        // Start is called before the first frame update
-        void Start()
+        spriteRenderer = GetComponent<SpriteRenderer>(); // we are accessing the SpriteRenderer that is attached to the Gameobject
+        if (spriteRenderer.sprite == null) // if the sprite on spriteRenderer is null then
+            spriteRenderer.sprite = sprite1; // set the sprite to sprite1
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sidewayy();
+
+        if (redmode == true)
         {
-            spriteRenderer = GetComponent<SpriteRenderer>(); // we are accessing the SpriteRenderer that is attached to the Gameobject
-            if (spriteRenderer.sprite == null) // if the sprite on spriteRenderer is null then
-                spriteRenderer.sprite = sprite1; // set the sprite to sprite1
-
-        }
-       
-        // Update is called once per frame
-        void Update()
-        {
-            sidewayy();
-          
-            if (redmode == true)
+            if (Input.GetKey(KeyCode.W))
             {
-                if (Input.GetKey(KeyCode.W))
-                {
-                    transform.position += new Vector3(0, 1, 0) * speed * Time.deltaTime;
-                }
-                if (Input.GetKey(KeyCode.S))
-                {
-                    transform.position += new Vector3(0, -1, 0) * speed * Time.deltaTime;
-                }
-
+                transform.position += new Vector3(0, 1, 0) * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += new Vector3(0, -1, 0) * speed * Time.deltaTime;
             }
 
+        }
+
+        if (redmode == false)
+        {
+
+            if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+            {
+                rb.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+                isGrounded = false;
+                Debug.Log("Jumped");
+                //jump mech
+            }
+
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.P) || _pushDownAttack._movementUpdate)
+        {
+            Debug.Log("a");
             if (redmode == false)
             {
-                
-                if (Input.GetKeyDown(KeyCode.W) && isGrounded)
-                {
-                    rb.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
-                    isGrounded = false;
-                    Debug.Log("Jumped");
-                    //jump mech
-                } 
-               
+                spriteRenderer.sprite = sprite1;
+                Debug.Log("Blue mode");
+                spriteswap();
+                redmode = true;
+                gravity.velocity = new Vector2(0, 0);
+                gravity.angularDrag = 0;
+                gravity.gravityScale = 0;
+
+                _pushDownAttack._movementUpdate = false;
             }
 
-
-
-            if (Input.GetKeyDown(KeyCode.P))
+            else if (redmode == true)
             {
-                Debug.Log("a");
-                if (redmode == false)
+                spriteRenderer.sprite = sprite2;
+                Debug.Log("Normal");
+                redmode = false;
+                gravity.angularDrag = 0.05f;
+                if (_pushDownAttack._movementUpdate == false)
                 {
-                    spriteRenderer.sprite = sprite1;
-                    Debug.Log("Blue mode");
-                    spriteswap();
-                    redmode = true;
-                    gravity.velocity = new Vector2(0, 0);
-                    gravity.angularDrag = 0;
-                    gravity.gravityScale = 0;
-
-
-                }
-                else if (redmode == true)
-                {
-                    spriteRenderer.sprite = sprite2;
-                    Debug.Log("Normal");
-                    redmode = false;
-                    gravity.angularDrag = 0.05f;
                     gravity.gravityScale = 1;
-
                 }
 
-
-
+                _pushDownAttack._movementUpdate = false;
             }
 
 
@@ -98,35 +99,46 @@ namespace movement2
 
 
 
-        void sidewayy()
+    }
+
+
+
+    void sidewayy()
+    {
+        if (Input.GetKey(KeyCode.A))
         {
-            if (Input.GetKey(KeyCode.A))
-            {
 
-                transform.position += new Vector3(-1, 0, 0) * speed * Time.deltaTime;
+            transform.position += new Vector3(-1, 0, 0) * speed * Time.deltaTime;
 
 
-            }
-
-
-            if (Input.GetKey(KeyCode.D))
-            {
-
-                transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
-
-            }
         }
-        public void spriteswap()
+
+
+        if (Input.GetKey(KeyCode.D))
         {
-          
+
+            transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+
         }
-       
-        private void OnCollisionEnter2D(Collision2D other)
+    }
+    public void spriteswap()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Border")
         {
-            if(other.gameObject.tag == "Border")
-            {
-                isGrounded = true;
-            }
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            isGrounded = false;
         }
     }
 }
